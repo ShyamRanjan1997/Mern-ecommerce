98c4fdfe774ab879eda7299625cf6421d9dd3650
ShyamRanjan1997 <83160908+ShyamRanjan1997@users.noreply.github.com>
2021-09-29 09:14:25 +0530

    Delete ecommerce-backend directory

--- ecommerce-backend/src/routes/cart.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { requireSignin, userMiddleware } = require('../common-middleware');
-const { addItemsToCart } = require('../controller/cart');
-const router = express.Router();
-
-router.post("/user/cart/addtocart", requireSignin, userMiddleware, addItemsToCart);
-
-module.exports = router;
